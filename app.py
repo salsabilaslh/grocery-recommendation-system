99088b912e8a950ce83f15b07877d283ac8eec55
살사 <salsabilaa1903@gmail.com>
2026-06-04 14:54:02 +0900

    Refactor app.py for improved readability and updates

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 plt.rcParams['font.family'] = 'Malgun Gothic'
 
-df = pd.read_csv(r"E:\salsa's\univ\3rd semester\AI\final project\association rules.csv")
+df = pd.read_csv("association rules.csv")
 df = df.iloc[2:].copy()
 numeric_cols = ["Support","Confidence","Coverage","Strength","Lift","Leverage"]
 
