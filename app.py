975dbe97336a56327e46dfd2605e4d639690418e
살사 <salsabilaa1903@gmail.com>
2026-06-04 16:05:37 +0900

    Refactor app.py for improved readability and updates

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-plt.rcParams['font.family'] = 'Malgun Gothic'
+plt.rcParams['font.family'] = 'DejaVu Sans'
+plt.rcParams['axes.unicode_minus'] = False
 
 df = pd.read_csv("association rules.csv")
 df = df.iloc[2:].copy()
